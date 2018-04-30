86eb7fc0d79b8fad7092c011bb11cffad96175c5
numaru <kin.numaru@protonmail.com>
2018-04-30 16:09:31 +0200

    Add list_iterator tests

--- test/test_list/main.c
+++ test/test_list/main.c
@@ -86,19 +86,77 @@ test_list_append()
 static void
 test_list_iterator_init_and_cleanup(void)
 {
-  test_expect(1 == 0);
+  struct pool pool;
+  struct list_node nodes[4];
+  struct list list;
+  struct list_iterator it;
+  signed variable = 0xAA;
+  test_expect(pool_init(&pool, nodes, sizeof(nodes), sizeof(nodes[0])) == 0);
+  test_expect(list_init(&list, &pool) == 0);
+  test_expect(list_iterator_init(&it, &list) == 0);
+  test_expect(list_append(&list, &variable) == 0);
+  test_expect(list_iterator_init(&it, &list) == 0);
+  test_expect(list_iterator_cleanup(&it) == 0);
+  test_expect(list_cleanup(&list) == 0);
+  test_expect(pool_cleanup(&pool) == 0);
 }
 
 static void
 test_list_iterator_get_data(void)
 {
-
+  struct pool pool;
+  struct list_node nodes[4];
+  struct list list;
+  struct list_iterator it;
+  signed variable = 0xAA;
+  signed * input = &variable;
+  signed * output = input + 0xBB;
+  test_expect(pool_init(&pool, nodes, sizeof(nodes), sizeof(nodes[0])) == 0);
+  test_expect(list_init(&list, &pool) == 0);
+  test_expect(list_iterator_init(&it, &list) == 0);
+  test_expect(list_iterator_get_data(&it, (void**)&output) == 1);
+  test_expect(output == NULL);
+  test_expect(list_append(&list, input) == 0);
+  test_expect(list_iterator_get_data(&it, (void**)&output) == 0);
+  test_expect(output == input);
+  test_expect(list_iterator_init(&it, &list) == 0);
+  test_expect(list_iterator_cleanup(&it) == 0);
+  test_expect(list_cleanup(&list) == 0);
+  test_expect(pool_cleanup(&pool) == 0);
 }
 
 static void
 test_list_iterator_get_next(void)
 {
-
+  struct pool pool;
+  struct list_node nodes[4];
+  struct list list;
+  struct list_iterator first_it;
+  struct list_iterator second_it;
+  signed variables[3] = {0xAA, 0xBB, 0xCC};
+  signed * output = &variables[0] + 0xBB;
+  test_expect(pool_init(&pool, nodes, sizeof(nodes), sizeof(nodes[0])) == 0);
+  test_expect(list_init(&list, &pool) == 0);
+  test_expect(list_iterator_init(&first_it, &list) == 0);
+  test_expect(list_append(&list, &variables[0]) == 0);
+  test_expect(list_append(&list, &variables[1]) == 0);
+  test_expect(list_append(&list, &variables[2]) == 0);
+  test_expect(list_iterator_get_data(&first_it, (void**)&output) == 0);
+  test_expect(output == &variables[0]);
+  test_expect(list_iterator_get_next(&first_it, &second_it) == 0);
+  test_expect(list_iterator_get_data(&first_it, (void**)&output) == 0);
+  test_expect(output == &variables[0]);
+  test_expect(list_iterator_get_data(&second_it, (void**)&output) == 0);
+  test_expect(output == &variables[1]);
+  test_expect(list_iterator_get_next(&second_it, &second_it) == 0);
+  test_expect(list_iterator_get_data(&second_it, (void**)&output) == 0);
+  test_expect(output == &variables[2]);
+  test_expect(list_iterator_get_data(&first_it, (void**)&output) == 0);
+  test_expect(output == &variables[0]);
+  test_expect(list_iterator_cleanup(&first_it) == 0);
+  test_expect(list_iterator_cleanup(&second_it) == 0);
+  test_expect(list_cleanup(&list) == 0);
+  test_expect(pool_cleanup(&pool) == 0);
 }
 
 static const test_step_t test_steps[] = {
